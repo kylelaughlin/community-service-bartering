87fbdd0f130333eb5acba5bac849da008370c174
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-05-10 13:43:21 -0500

    User can now sign up and save with encrypted apssword

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ActiveRecord::Base
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
-  validates :email, presence: true, uniquiness: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
 end
