37ebdd1feb3e8fa8781a8127e6e6bfbd6fa486c5
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-05-12 09:54:37 -0500

    added model tests for creation and presence validations

--- test/models/request_test.rb
+++ test/models/request_test.rb
@@ -4,4 +4,31 @@ class RequestTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "request object valid upon normal creation" do
+    @request = Request.new(title: "Example Title", description: "Need work done on house",
+                            user_id: 1)
+    assert(@request.valid?, "Should be valid under normal circumstances")
+  end
+
+  test "Should be invalid without title" do
+    @request = Request.new(title: "Example Title", description: "Need work done on house",
+                            user_id: 1)
+    @request.title = nil
+    refute(@request.valid?, "Should be invalid with a nil title")
+  end
+
+  test "Should be invalid without description" do
+    @request = Request.new(title: "Example Title", description: "Need work done on house",
+                            user_id: 1)
+    @request.description = nil
+    refute(@request.valid?, "Should be invalid with a nil description")
+  end
+
+  test "Should be invalid without user_id" do
+    @request = Request.new(title: "Example Title", description: "Need work done on house",
+                            user_id: 1)
+    @request.user_id = nil
+    refute(@request.valid?, "Should be invalid with a nil user_id")
+  end
 end
