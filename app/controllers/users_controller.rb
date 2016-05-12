8db0c6a0dbc20a9104ecefbcb5272c6ea0ae26e0
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-05-12 14:14:41 -0500

    added auto login upon user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      auto_login(@user)
       redirect_to user_path(@user.id)
     else
       render :new
