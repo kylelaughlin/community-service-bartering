2bcda45f7666d3891b271aa3810fd8481203cf48
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-05-13 11:33:29 -0500

    add partial form to edit page and update styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,18 +21,22 @@
 #
 
 class User < ActiveRecord::Base
+  #Sorcery Authentification
   authenticates_with_sorcery!
 
+  # Validates password length and confirmation
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
+  #database validations
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :first_name, presence: true
   validates :last_name, presence: true
 
   has_many :requests
 
+  # Attached file through paperclip with AWS S3 storage
   has_attached_file :avatar,
                     styles: { medium: "300x300>", thumb: "100x100>" },
                     default_url: "/images/:style/missing.png",
@@ -40,5 +44,6 @@ class User < ActiveRecord::Base
                     s3_credentials: {access_key_id: ENV["AWS_ACCESS_KEY_ID"],
                                      secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]},
                     bucket: ENV["S3_BUCKET_NAME"]
+                    
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
