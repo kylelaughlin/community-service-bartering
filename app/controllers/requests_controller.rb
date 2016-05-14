7cc80e99587efc871d21d26a417e8b8e5e6c6b15
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-05-14 10:41:57 -0500

    add form to new request view WIP

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,14 +1,34 @@
 class RequestsController < ApplicationController
   def new
+    @request = Request.new
+  end
+
+  def create
+    byebug
+    @request = Request.new(request_params)
+    if @request.save
+      redirect_to request_path(@request.id), notice: "Request Created"
+    else
+      flash.now[:alert] = "Request Not Created"
+      render :new
+    end
   end
 
   def show
+    @request = Request.find(params[:id])
   end
 
   def index
+    @request.all
   end
 
   def edit
+  end
+
+  private
+
+  def request_params
+    params.require(:request).permit(:title, :description, :user_id)
   end
 
 end
