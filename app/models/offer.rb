1d59a4b38763d21593f8a84f7f4fd6bb6c4b673a
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-05-15 16:30:31 -0500

    add numericality tests to offer model tests

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -12,4 +12,9 @@
 #
 
 class Offer < ActiveRecord::Base
+
+  validates :proposed_hours, presence: true, numericality: {greater_than_or_equal_to: 0}
+  validates :user_id, presence: true
+  validates :request_id, presence: true
+  
 end
--- test/models/offer_test.rb
+++ test/models/offer_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class OfferTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "offer valid under normal circumstances" do
+    @offer = Offer.new(proposed_hours: 2, user_id: 1, request_id: 2)
+    assert(@offer.valid?, "Should be valid offer under normal circumstances")
+  end
+
+  test "should be invalid with no user_id" do
+    @offer = Offer.new(proposed_hours: 2, user_id: 1, request_id: 2)
+    @offer.user_id = nil
+    refute(@offer.valid?, "Should be invalid offer with no user_id")
+  end
+
+  test "should be invalid with no request_id" do
+    @offer = Offer.new(proposed_hours: 2, user_id: 1, request_id: 2)
+    @offer.request_id = nil
+    refute(@offer.valid?, "Should be invalid offer with no request_id")
+  end
+
+  test "should be invalid with no proposed_hours" do
+    @offer = Offer.new(proposed_hours: 2, user_id: 1, request_id: 2)
+    @offer.proposed_hours = nil
+    refute(@offer.valid?, "Should be invalid offer with no proposed_hours")
+  end
+
+  test "should be invalid with negative proposed_hours" do
+    @offer = Offer.new(proposed_hours: 2, user_id: 1, request_id: 2)
+    @offer.proposed_hours = -2
+    refute(@offer.valid?, "Should be invalid offer with a negative proposed_hours")
+  end
+
+  test "should be valid offer with proposed_hours" do
+    @offer = Offer.new(proposed_hours: 2, user_id: 1, request_id: 2)
+    @offer.proposed_hours = 0
+    assert(@offer.valid?, "Should be valid offer with zero proposed_hours")
+  end
+
 end
