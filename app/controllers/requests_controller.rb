679ea27d38f58aaee4dfa32cd52f59aa73bb5ce9
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-05-15 15:19:50 -0500

    Update styling to edit request form; add destroy controller action for request

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -4,10 +4,10 @@ class RequestsController < ApplicationController
 
   def new
     @request = Request.new
-    user_allowed?
   end
 
   def create
+    byebug
     @request = Request.new(request_params)
     if @request.save
       if params[:images]
@@ -36,6 +36,27 @@ class RequestsController < ApplicationController
     @request = Request.find(params[:id])
     @user = @request.user
     user_allowed?
+  end
+
+  def update
+    @request = Request.find(params[:id])
+    if @request.update_attributes(request_params)
+      if params[:images]
+        params[:images].each do |image|
+          RequestImage.create(image: image, request: @request)
+        end
+      end
+      redirect_to request_path(@request.id), notice: "Request Created"
+    else
+      flash.now[:alert] = "Request Not Created"
+      render :edit
+    end
+  end
+
+  def destroy
+    @request = Request.find(params[:id])
+    @request.destroy
+    redirect_to user_path(current_user.id)
   end
 
   private
