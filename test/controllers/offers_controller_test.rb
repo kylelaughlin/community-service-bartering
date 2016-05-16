60344804a9a211e475ac8caf9a4b29bfaf078786
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-05-15 21:52:42 -0500

    update offer controller tests

--- test/controllers/offers_controller_test.rb
+++ test/controllers/offers_controller_test.rb
@@ -2,22 +2,36 @@ require 'test_helper'
 
 class OffersControllerTest < ActionController::TestCase
   test "should get new" do
-    get :new
+    @user = users(:default_user)
+    login_user(user=@user, route=login_path)
+    @default_request = requests(:default_request)
+    get :new, :request_id => @default_request.id
     assert_response :success
   end
 
   test "should get index" do
-    get :index
+    @user = users(:default_user)
+    login_user(user=@user, route=login_path)
+    @default_request = requests(:default_request)
+    get :index, :request_id => @default_request.id
     assert_response :success
   end
 
   test "should get show" do
-    get :show
+    @user = users(:default_user)
+    login_user(user=@user, route=login_path)
+    @default_request = requests(:default_request)
+    @default_offer = offers(:default_offer)
+    get :show, :request_id => @default_request.id, :id => @default_offer.id
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit
+    @user = users(:default_user)
+    login_user(user=@user, route=login_path)
+    @default_request = requests(:default_request)
+    @default_offer = offers(:default_offer)
+    get :edit, :request_id => @default_request.id, :id => @default_offer.id
     assert_response :success
   end
 
