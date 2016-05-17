fc4c7c18f59d0f251173600ce7e2c59f28d6c98f
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-05-16 20:53:24 -0500

    styled new rating form

--- app/assets/javascripts/ratings.js
+++ app/assets/javascripts/ratings.js
@@ -1,2 +1,84 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(function() {
+    $("#star-1").hover(
+      function(){
+        $(this).addClass("star-hover");
+      }, function(){
+        $(this).removeClass("star-hover");
+      }
+    );
+
+    $("#star-2").hover(
+      function(){
+        $("#star-1, #star-2").addClass("star-hover");
+      }, function(){
+        $("#star-1, #star-2").removeClass("star-hover");
+      }
+    );
+
+    $("#star-3").hover(
+      function(){
+        $("#star-1, #star-2, #star-3").addClass("star-hover");
+      }, function(){
+        $("#star-1, #star-2, #star-3").removeClass("star-hover");
+      }
+    );
+
+    $("#star-4").hover(
+      function(){
+        $("#star-1, #star-2, #star-3, #star-4").addClass("star-hover");
+      }, function(){
+        $("#star-1, #star-2, #star-3, #star-4").removeClass("star-hover");
+      }
+    );
+
+    $("#star-5").hover(
+      function(){
+        $("#star-1, #star-2, #star-3, #star-4, #star-5").addClass("star-hover");
+      }, function(){
+        $("#star-1, #star-2, #star-3, #star-4, #star-5").removeClass("star-hover");
+      }
+    );
+
+    $("#star-1").click(
+      function(){
+        $("#star-1, #star-2, #star-3, #star-4, #star-5").removeClass("star-selected");
+        $(this).addClass("star-selected");
+        $("#rating_rating").val(1);
+      }
+    );
+
+    $("#star-2").click(
+      function(){
+        $("#star-1, #star-2, #star-3, #star-4, #star-5").removeClass("star-selected");
+        $("#star-1, #star-2").addClass("star-selected");
+        $("#rating_rating").val(2);
+      }
+    );
+
+    $("#star-3").click(
+      function(){
+        $("#star-1, #star-2, #star-3, #star-4, #star-5").removeClass("star-selected");
+        $("#star-1, #star-2, #star-3").addClass("star-selected");
+        $("#rating_rating").val(3);
+      }
+    );
+
+    $("#star-4").click(
+      function(){
+        $("#star-1, #star-2, #star-3, #star-4, #star-5").removeClass("star-selected");
+        $("#star-1, #star-2, #star-3, #star-4").addClass("star-selected");
+        $("#rating_rating").val(4);
+      }
+    );
+
+    $("#star-5").click(
+      function(){
+        $("#star-1, #star-2, #star-3, #star-4, #star-5").addClass("star-selected");
+        $("#rating_rating").val(5);
+      }
+    );
+
+});
--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,8 +1,21 @@
 class RatingsController < ApplicationController
   def new
+    @rating = Rating.new
+    @user = User.find(params[:request_user_id])
+    @offer_user = User.find(params[:user_id])
+    user_allowed?
+  end
+
+  def create
     byebug
   end
 
   def show
+  end
+
+  private
+
+  def ratings_params
+    params.require(:rating).permit(:user_id, :rating)
   end
 end
