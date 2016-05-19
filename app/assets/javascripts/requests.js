5da9fda7f32e15e0f58f7a3d4490ef18f7108412
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-05-19 13:26:52 -0500

    remove unused javascript

--- app/assets/javascripts/requests.js
+++ app/assets/javascripts/requests.js
@@ -1,12 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$(function() {
-  $("#search_form input").keyup(function() {
-    $.get($("#search_form").attr("action"), $("#search_form").serialize(), null, "script");
-    return false;
-  });
-});
 
 //index.js.erb (same folder as index.html.erb)
 var request_summary = "request_summary"
-$("#bands").html("<%= escape_javascript(render(request_summary)) %>");
+$("#request-list").html("<%= escape_javascript(render(request_summary)) %>");
